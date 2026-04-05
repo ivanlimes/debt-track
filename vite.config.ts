dd164e470f31a07c2b7a8b4499beb275c2765bd2
ivanlimes <ivangaleano930@gmail.com>
2026-04-05 16:32:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/debt-track/",
+  base: "/Debt-Track/",
   plugins: [react()],
 });
